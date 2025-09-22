7e2755696b01a72cf110e63fc6e861b7a9562983
Mariana Vallejos <mariana.vallejos@assuresoft.tech>
2025-09-22 10:11:26 -0700

    feat: use formit and yup schema validation in rating form component

--- src/components/RatingComponent.tsx
+++ src/components/RatingComponent.tsx
@@ -1,26 +1,32 @@
-import { useState } from "react";
+import { useState, type FC } from "react";
 import { FiStar } from "react-icons/fi";
-export default function RatingComponent() {
-    const [stars, setStarts] = useState(0);
+
+interface RatingComponentProps {
+    rating: number;
+    setRating: (rating: number) => void;
+}
+
+const RatingComponent: FC<RatingComponentProps> = ({ setRating, rating}) => {
+
     return (
-        <div className="flex bg-white items-center  min-w-[600px] py-2">
-            <div className="py-2 text-base font-semibold">
-                Rate:
-            </div>
+        <div className="flex bg-white items-center min-w-[600px] py-2">
+            <div className="py-2 text-base font-semibold">Rate:</div>
             <div className="flex gap-4 p-2">
                 {Array.from({ length: 5 }).map((_, index) => (
                     <div key={index} className="flex justify-center px-2">
                         <FiStar
                             size={25}
                             strokeWidth={0}
-                            fill={index + 1 <= stars ? "gold" : "#D6DBDF"}
+                            fill={index + 1 <= rating ? "gold" : "#D6DBDF"}
                             cursor="pointer"
                             className="star"
-                            onClick={() => setStarts(index + 1)}
+                            onClick={() => setRating(index + 1)}
                         />
                     </div>
                 ))}
             </div>
         </div>
     );
-}
+};
+
+export default RatingComponent;
--- src/components/RatingForm.tsx
+++ src/components/RatingForm.tsx
@@ -1,29 +1,104 @@
-import RatingComponent from "./RatingComponent"
+import { useFormik, type FormikHelpers } from "formik";
+import * as Yup from "yup";
+import RatingComponent from "./RatingComponent";
+import { useState } from "react";
 
-const RatingForm = () => {
-    return (
-        <section className="flex p-10 gap-4 mx-10">
-            <div className="flex flex-col w-1/2 px-5">
-                <h3 className="text-2xl font-bold">Rating form</h3>
-                <div className="flex gap-2 items-center py-2">
-                    <label htmlFor="nameRatingForm" className="font-semibold">Name:</label>
-                    <input type="text" className="border my-2 rounded px-2" placeholder="Introduce your name here" id="nameRatingForm" />
-                </div>
-                <div className="">
-                    <RatingComponent/>
-                </div>
-                <div className="flex gap-2 py-2">
-
-                    <label htmlFor="messageRatingForm" className="font-semibold">Message:</label>
-                    <textarea name="message" id="messageRatingForm" className="border rounded w-2/3" rows={5} placeholder="Write your message"></textarea>
-                </div>
-            </div>
-            <div className="w-1/2 px-5">
-                <h4 className="text-xl font-semibold">Information submitted:</h4>
-                <p></p>
-            </div>
-        </section>
-    )
+interface FormValues {
+    name: string;
+    rating: number;
+    message: string;
 }
 
-export default RatingForm
+const initialValues: FormValues = {
+    name: '',
+    rating: 0,
+    message: ''
+};
+
+const RatingFormSchema = Yup.object().shape({
+    name: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Name is required'),
+    rating: Yup.number()
+        .min(1, 'Please rate between 1 and 5')
+        .max(5, 'Please rate between 1 and 5')
+        .required('Rating is required'),
+    message: Yup.string().when("rating", {
+        is: (val: number) => val < 3,
+        then: (schema) => schema.required("Message is required when rating is less than 3"),
+        otherwise: (schema) => schema.notRequired(),
+    }),
+});
+
+const RatingForm = () => {
+    const [dataSubmited, setDataSubmited] = useState<string>()
+    const formik = useFormik<FormValues>({
+        initialValues,
+        validationSchema: RatingFormSchema,
+        onSubmit: (values: FormValues, { resetForm }: FormikHelpers<FormValues>) => {
+            setDataSubmited(JSON.stringify(values, null, 2));
+            resetForm();
+        },
+    });
+
+    return (
+        <section className="flex p-10 gap-4 mx-10">
+            <form onSubmit={formik.handleSubmit} className="flex flex-col w-1/2 px-5">
+                <h3 className="text-2xl font-bold">Rating form</h3>
+
+                <div className="flex gap-2 items-center py-2">
+                    <label htmlFor="nameRatingForm" className="font-semibold">Name:</label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="nameRatingForm"
+                        className="border my-2 rounded px-2"
+                        placeholder="Introduce your name here"
+                        onChange={formik.handleChange}
+                        value={formik.values.name}
+                    />
+                </div>
+                {formik.touched.name && formik.errors.name && (
+                    <div className="text-red-500 text-sm">{formik.errors.name}</div>
+                )}
+
+                <div className="py-2">
+                    <RatingComponent setRating={(val) => formik.setFieldValue("rating", val)} rating={formik.values.rating} />
+                </div>
+                {formik.touched.rating && formik.errors.rating && (
+                    <div className="text-red-500 text-sm">{formik.errors.rating}</div>
+                )}
+
+                <div className="flex gap-2 py-2">
+                    <label htmlFor="messageRatingForm" className="font-semibold">Message:</label>
+                    <textarea
+                        name="message"
+                        id="messageRatingForm"
+                        className="border rounded w-2/3"
+                        rows={5}
+                        placeholder="Write your message"
+                        onChange={formik.handleChange}
+                        value={formik.values.message}
+                    ></textarea>
+                </div>
+                {formik.touched.message && formik.errors.message && (
+                    <div className="text-red-500 text-sm">{formik.errors.message}</div>
+                )}
+
+                <div className="mx-auto py-3">
+                    <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded">
+                        Submit
+                    </button>
+                </div>
+            </form>
+
+            <div className="w-1/2 px-5">
+                <h4 className="text-xl font-semibold">Information submitted:</h4>
+                <p>{dataSubmited}</p>
+            </div>
+        </section>
+    );
+};
+
+export default RatingForm;
